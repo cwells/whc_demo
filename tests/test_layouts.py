9c18ffe0e5c8df011e14534e1b68e80847cef7ae
Cliff Wells <cliff.wells@gmail.com>
2024-11-13 18:29:46 -0800

    Test layout

---
+++ tests/test_layouts.py
@@ -0,0 +1,22 @@
+from dash.development.base_component import Component
+
+from ..app import map_layout, import_dataframe, WHC_DATA
+
+def recurse(lst, test):
+    """ Recursively walk nested list, performing test on each item """
+
+    for item in lst:
+        if isinstance(item, list):
+            recurse(item)
+        else:
+            yield test(item)
+
+
+def test_map_layout():
+    """ Ensure the map_layout returns one or more Components """
+
+    df = import_dataframe(WHC_DATA)
+    layout = map_layout(df, lang="en")
+
+    assert len(layout)
+    assert all(recurse(layout, test=lambda i: isinstance(i, Component)))
